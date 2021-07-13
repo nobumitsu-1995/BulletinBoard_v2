5f3eaabfc07341c0a787da392df58d3942d30237
masugata1995 <masugata1995@gmail.com>
2021-07-13 22:29:04 +0900

    edit migration

--- db/migrate/20210704100344_create_users.rb
+++ db/migrate/20210704100344_create_users.rb
@@ -4,7 +4,6 @@ class CreateUsers < ActiveRecord::Migration[6.1]
       t.string :provider, null: false
       t.string :uid, null:false
       t.string :name, null:false
-      t.string :email, null:false
       t.string :avatar, null:false
 
       t.timestamps
